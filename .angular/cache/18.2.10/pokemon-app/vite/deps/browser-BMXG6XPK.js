f3abcaa946fe4e141fef7227877e7dbf055d159c
williamanoell <40405535+williamanoell@users.noreply.github.com>
2024-10-28 11:16:14 -0300

    Adicionando a pasta .angular/cache ao repositório

---
+++ .angular/cache/18.2.10/pokemon-app/vite/deps/browser-BMXG6XPK.js
@@ -0,0 +1,53 @@
+import {
+  Animation,
+  AnimationDriver,
+  AnimationEngine,
+  AnimationRenderer,
+  AnimationRendererFactory,
+  AnimationStyleNormalizer,
+  BaseAnimationRenderer,
+  NoopAnimationDriver,
+  NoopAnimationStyleNormalizer,
+  WebAnimationsDriver,
+  WebAnimationsPlayer,
+  WebAnimationsStyleNormalizer,
+  allowPreviousPlayerStylesMerge,
+  camelCaseToDashCase,
+  containsElement,
+  createEngine,
+  getParentElement,
+  invokeQuery,
+  normalizeKeyframes,
+  validateStyleProperty,
+  validateWebAnimatableStyleProperty
+} from "./chunk-26GYRJJ7.js";
+import "./chunk-Y5ERQFYA.js";
+import "./chunk-3BKIY7M2.js";
+import "./chunk-MJLRC3IY.js";
+import "./chunk-ZILRDZYF.js";
+import "./chunk-HHDBSK6W.js";
+import "./chunk-32LHFCYG.js";
+export {
+  AnimationDriver,
+  NoopAnimationDriver,
+  Animation as ɵAnimation,
+  AnimationEngine as ɵAnimationEngine,
+  AnimationRenderer as ɵAnimationRenderer,
+  AnimationRendererFactory as ɵAnimationRendererFactory,
+  AnimationStyleNormalizer as ɵAnimationStyleNormalizer,
+  BaseAnimationRenderer as ɵBaseAnimationRenderer,
+  NoopAnimationStyleNormalizer as ɵNoopAnimationStyleNormalizer,
+  WebAnimationsDriver as ɵWebAnimationsDriver,
+  WebAnimationsPlayer as ɵWebAnimationsPlayer,
+  WebAnimationsStyleNormalizer as ɵWebAnimationsStyleNormalizer,
+  allowPreviousPlayerStylesMerge as ɵallowPreviousPlayerStylesMerge,
+  camelCaseToDashCase as ɵcamelCaseToDashCase,
+  containsElement as ɵcontainsElement,
+  createEngine as ɵcreateEngine,
+  getParentElement as ɵgetParentElement,
+  invokeQuery as ɵinvokeQuery,
+  normalizeKeyframes as ɵnormalizeKeyframes,
+  validateStyleProperty as ɵvalidateStyleProperty,
+  validateWebAnimatableStyleProperty as ɵvalidateWebAnimatableStyleProperty
+};
+//# sourceMappingURL=browser-BMXG6XPK.js.map
